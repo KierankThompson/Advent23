3d2f1122abf1310d92e33aae6d04d30aa4d89cdb
KierankThompson <thompson.kierank@gmail.com>
2023-12-04 05:26:53 -0500

    Solution for day 3 part 1

--- advent3Part1.py
+++ advent3Part1.py
@@ -1,12 +1,41 @@
-s = open("advent3.txt","r")
+s2 = open("advent3.txt","r")
+s = s2.readlines()
+
 directions = ((0, -1), (0, 1), (-1, 0), (1, 0),(1,1),(-1,-1),(1,-1),(-1,1))
 ans = 0
 counter = 0
-dic = {}
-for i in s:
-    counter2 = 0
-    for j in i:
+for i in range(len(s)):
+    lastNum = ""
+    for j in range(len(s[i])):
+        if s[i][j].isdigit():
+            for d in directions:
+                positionX = i+d[0]
+                positionY = j+d[1]
+                if 0 <= positionX < len(s) and 0 <= positionY < len(s[i]):
+                    if not s[positionX][positionY].isdigit() and s[positionX][positionY] != "." and s[positionX][positionY] != "\n":
+                        curDigit = s[i][j]
+                        for mi in range(j-1,-1,-1):
+                            if s[i][mi].isdigit():
+                                curDigit = s[i][mi] + curDigit
+                            else:
+                                break
+                        for ma in range(j+1,len(s[i])):
+                            if s[i][ma].isdigit():
+                                curDigit += s[i][ma] 
+                            else:
+                                break
+                        #print("\nThis is lastNum " + lastNum)
+                        #print("This is curDigit " + curDigit)
+                        if curDigit != lastNum:
+                            #print(curDigit)
+                            ans += int(curDigit)
+                            lastNum = curDigit
+                            break
+        else:
+            lastNum = ""
+print(ans)
+
         
 
-print(ans)
+
             
